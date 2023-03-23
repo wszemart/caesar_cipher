aa6301f6704dd9edd09deda5f3ea9ca827360b31
wszemart <wszeborska@wp.pl>
2023-03-23 11:19:32 +0100

    Change encrypt/decrypt method

--- cipher.py
+++ cipher.py
@@ -1,4 +1,3 @@
-import codecs
 from typing import Union
 
 """ class to encrypt and decrypt text on dependency to choose ROT13/ROT47 """
@@ -10,7 +9,16 @@ class Cipher:
 
     def encrypt(self, text: str, rot_type: str) -> Union[str, None]:
         if rot_type == self.ROT13:
-            return codecs.encode(text, 'rot13')
+            encrypted_text: str = ''
+            for char in text:
+                char_code: int = ord(char)
+                if 33 <= char_code <= 126:
+                    encrypted_char_code = (char_code + 13 - 33) % 94 + 33
+                    encrypted_char = chr(encrypted_char_code)
+                    encrypted_text += encrypted_char
+                else:
+                    encrypted_text += char
+            return encrypted_text
         elif rot_type == self.ROT47:
             encrypted_text: str = ''
             for char in text:
@@ -26,18 +34,24 @@ class Cipher:
             return None
 
     def decrypt(self, text: str, rot_type: str) -> Union[str, None]:
-        if rot_type == self.ROT13:
-            return codecs.encode(text, 'rot13')
-        elif rot_type == self.ROT47:
-            decrypted_text: str = ''
+        if rot_type == "rot47":
+            return self.encrypt(text, rot_type)
+        else:
+            encrypted_text: str = ''
             for char in text:
                 char_code: int = ord(char)
                 if 33 <= char_code <= 126:
-                    decrypted_char_code: int = (char_code - 47 - 33) % 94 + 33
-                    decrypted_char = chr(decrypted_char_code)
-                    decrypted_text += decrypted_char
+                    encrypted_char_code = (char_code - 13 - 33) % 94 + 33
+                    encrypted_char = chr(encrypted_char_code)
+                    encrypted_text += encrypted_char
                 else:
-                    decrypted_text += char
-            return decrypted_text
-        else:
-            return None
+                    encrypted_text += char
+            return encrypted_text
+
+
+test = Cipher()
+tekst = "Marta jest chora"
+print(test.encrypt(tekst, "rot13"))
+print(test.decrypt('Zn!#n wr"# pu|!n', "rot13"))
+print(test.encrypt(tekst, "rot47"))
+print(test.decrypt("|2CE2 ;6DE 49@C2", "rot47"))
