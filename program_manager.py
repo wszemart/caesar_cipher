57c22ceb600a94cdaaee4b009114d78bb1f3ca31
wszemart <wszeborska@wp.pl>
2023-03-24 18:26:10 +0100

    Connect menu with manager

--- program_manager.py
+++ program_manager.py
@@ -1,12 +1,13 @@
 from cipher import Cipher
 from file_handler import FileHandler
-from menu import Menu
+from program_menu import *
 
 
 class Manager:
     def __init__(self):
         self.ceasar_cipher = Cipher()
         self.file_handler = FileHandler()
+        self.menu = Menu()
         self.__is_running = True
         self.options = {
             1: self.ceasar_cipher.encrypt,
@@ -16,21 +17,20 @@ class Manager:
             5: self.exit_program
         }
 
+    @staticmethod
+    def start():
+        return Manager.show_menu()
+
+    @staticmethod
+    def show_menu():
+        return Menu.show_menu()
+
     def show_error(self):
         print("Error!")
 
-    def execute(self, choice: int):
-        self.options.get(choice, self.show_error)()
+    def execute(self):
+        self.options.get(Menu.get_choice(), self.show_error)()
 
     def exit_program(self):
         print("Exit the programme")
         self.__is_running = False
-
-
-def main():
-    menu = Menu()
-    menu.show_menu()
-
-
-if __name__ == '__main__':
-    main()
