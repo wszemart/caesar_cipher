ae8280106bf998f434659c470530e889da2bfd21
wszemart <wszeborska@wp.pl>
2023-03-27 13:15:24 +0200

    Add class to keep the data

--- text.py
+++ text.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
 
+""" data class """
+
 
 @dataclass
 class Text:
     text: str
     rot_type:  str
     status: str
+    write_to_file: str
+    read_from_file: str
