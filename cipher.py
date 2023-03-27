59b77a87d4a7cb2a3e964f528f3f6dc7b3140d7f
wszemart <wszeborska@wp.pl>
2023-03-27 13:27:55 +0200

    Add abstract factory

--- cipher.py
+++ cipher.py
@@ -1,49 +1,68 @@
-from typing import Union
+from typing import Union, Optional
+from abc import ABC, abstractmethod
 
 """ class to encrypt and decrypt text on dependency to choose ROT13/ROT47 """
 
 
-class Cipher:
-    ROT13 = 'rot13'
-    ROT47 = 'rot47'
+class Rot(ABC):
+    ROT13 = "rot13"
+    ROT47 = "rot47"
 
-    def encrypt(self, text: str, rot_type: str) -> Union[str, None]:
-        if rot_type == self.ROT13:
-            encrypted_text: str = ''
-            for char in text:
-                char_code: int = ord(char)
-                if 33 <= char_code <= 126:
-                    encrypted_char_code = (char_code + 13 - 33) % 94 + 33
-                    encrypted_char = chr(encrypted_char_code)
-                    encrypted_text += encrypted_char
-                else:
-                    encrypted_text += char
-            return encrypted_text
-        elif rot_type == self.ROT47:
-            encrypted_text: str = ''
-            for char in text:
-                char_code: int = ord(char)
-                if 33 <= char_code <= 126:
-                    encrypted_char_code = (char_code + 47 - 33) % 94 + 33
-                    encrypted_char = chr(encrypted_char_code)
-                    encrypted_text += encrypted_char
-                else:
-                    encrypted_text += char
-            return encrypted_text
-        else:
-            return None
+    @abstractmethod
+    def encrypt(self, text: str) -> Optional[str]:
+        raise NotImplementedError
 
-    def decrypt(self, text: str, rot_type: str) -> Union[str, None]:
-        if rot_type == "rot47":
-            return self.encrypt(text, rot_type)
-        else:
-            encrypted_text: str = ''
-            for char in text:
-                char_code: int = ord(char)
-                if 33 <= char_code <= 126:
-                    encrypted_char_code = (char_code - 13 - 33) % 94 + 33
-                    encrypted_char = chr(encrypted_char_code)
-                    encrypted_text += encrypted_char
-                else:
-                    encrypted_text += char
-            return encrypted_text
+    @abstractmethod
+    def decrypt(self, text: str) -> Optional[str]:
+        raise NotImplementedError
+
+    def _apply_rotation(self, char_code: int, rot_value: int) -> int:
+        return (char_code + rot_value - 33) % 94 + 33
+
+    @staticmethod
+    def create_rot(rot_type: str):
+        if rot_type == Rot.ROT13:
+            return Rot13()
+        elif rot_type == Rot.ROT47:
+            return Rot47()
+
+
+class Rot13(Rot):
+    ROT_VALUE = 13
+
+    def encrypt(self, text: str) -> Union[str]:
+        return self._apply_rotation_to_text(text, self.ROT_VALUE)
+
+    def decrypt(self, text: str) -> Optional[str]:
+        return self._apply_rotation_to_text(text, -self.ROT_VALUE)
+
+    def _apply_rotation_to_text(self, text: str, rot_value: int) -> str:
+        result = ""
+        for char in text:
+            char_code: int = ord(char)
+            if 33 <= char_code <= 126:
+                encrypted_char_code = self._apply_rotation(char_code, rot_value)
+                encrypted_char = chr(encrypted_char_code)
+                result += encrypted_char
+            else:
+                result += char
+        return result
+
+
+class Rot47(Rot):
+    ROT_VALUE = 47
+
+    def encrypt(self, text: str) -> Union[str]:
+        encrypted_text: str = ""
+        for char in text:
+            char_code: int = ord(char)
+            if 33 <= char_code <= 126:
+                encrypted_char_code = (char_code + 47 - 33) % 94 + 33
+                encrypted_char = chr(encrypted_char_code)
+                encrypted_text += encrypted_char
+            else:
+                encrypted_text += char
+        return encrypted_text
+
+    def decrypt(self, text: str) -> Optional[str]:
+        return self.encrypt(text)
