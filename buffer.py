a25d886199f91ab8f0f143db241c4e4d5c30780c
wszemart <wszeborska@wp.pl>
2023-03-28 12:16:32 +0200

    Add class Text

--- buffer.py
+++ buffer.py
@@ -1,6 +1,10 @@
+from file_handler import *
 from typing import Optional
+from dataclasses import asdict
+from text import *
 
 """ class to handle all logs """
+# TODO Text klasa danych.
 
 
 class MemoryBuffer:
@@ -8,18 +12,34 @@ class MemoryBuffer:
 
     @staticmethod
     def create_log(
-        text: Optional[str],
-        rot_type: Optional[str],
-        status: Optional[str],
-        write_file: Optional[str] = "",
-        read_file: Optional[str] = "",
+        text: str = '',
+        rot_type: str = '',
+        status: str = '',
+        write_to_file: str = '',
+        read_from_file: str = '',
     ):
         MemoryBuffer.logs.append(
-            {
-                "text": text,
-                "rot_type": rot_type,
-                "status": status,
-                "write_to_file": write_file,
-                "read_from_file": read_file,
-            }
+            Text(text, rot_type, status, write_to_file, read_from_file)
         )
+
+    @staticmethod
+    def show_buffer():
+        for count, log in enumerate(MemoryBuffer.logs):
+            print(f'{count}. {log}')
+
+    @staticmethod
+    def clear_buffer():
+        MemoryBuffer.logs = []
+
+    @staticmethod
+    def del_position():
+        choice_to_del = int(input("What do you want to delete? Enter a number: "))
+        removed_element = MemoryBuffer.logs.pop(choice_to_del)
+        print(f'"{removed_element}" was removed.')
+
+    @staticmethod
+    def save_buffer_to_file():
+        filename = input("Enter file name to save Memory Buffer: ")
+        FileHandler.write_file(filename, MemoryBuffer.logs)
+
+
