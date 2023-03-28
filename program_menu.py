e06c5acb627e3ae199fbec6e73b3752ccca1d819
wszemart <wszeborska@wp.pl>
2023-03-28 12:15:36 +0200

    New menu

--- program_menu.py
+++ program_menu.py
@@ -6,11 +6,15 @@ class Menu:
     def show_menu():
         print(
             """Choose any option:
-            1. Encrypt text
-            2. Decrypt text
-            3. Write to file
-            4. Read from file
-            5. Exit
+            1. Enter text to encrypt
+            2. Enter text to decrypt
+            3. Enter data to write to file
+            4. Read data from file
+            5. Show Memory Buffer
+            6. Clear Memory Buffer
+            7. Delete position from Memory Buffer
+            8. Save Memory Buffer to file
+            9. Exit
         """
         )
 
