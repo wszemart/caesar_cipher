70066649f1ebbbafe1aed6d9ef22fe7510be8e11
wszemart <wszeborska@wp.pl>
2023-04-01 14:59:21 +0200

    Fix bugs

--- text.py
+++ text.py
@@ -1,12 +1,8 @@
 from dataclasses import dataclass
 
-""" data class """
-
 
 @dataclass
 class Text:
     text: str
     rot_type:  str
     status: str
-    write_to_file: str
-    read_from_file: str
