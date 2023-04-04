c47736d04583b989c342f39db622cb93173fa8c4
wszemart <wszeborska@wp.pl>
2023-04-04 19:57:18 +0200

    Add typing

--- buffer.py
+++ buffer.py
@@ -11,28 +11,28 @@ class MemoryBuffer:
         text: str = "",
         rot_type: str = "",
         status: str = "",
-    ):
+    ) -> None:
         MemoryBuffer.logs.append(Text(text, rot_type, status))
 
     @staticmethod
-    def show_buffer():
+    def show_buffer() -> None:
         for count, log in enumerate(MemoryBuffer.logs):
             print(f"{count}. {log}")
 
     @staticmethod
-    def clear_buffer():
+    def clear_buffer() -> None:
         MemoryBuffer.logs = []
 
     @staticmethod
-    def del_position():
+    def del_position() -> None:
         if MemoryBuffer.logs:
-            choice_to_del = int(input("What do you want to delete? Enter a number: "))
+            choice_to_del: int = int(input("What do you want to delete? Enter a number: "))
             removed_element = MemoryBuffer.logs.pop(choice_to_del)
             print(f'"{removed_element}" was removed.')
         else:
             print("Memory Buffer is empty!")
 
     @staticmethod
-    def save_buffer_to_file():
-        filename = input("Enter file name to save Memory Buffer: ")
+    def save_buffer_to_file() -> None:
+        filename: str = input("Enter file name to save Memory Buffer: ")
         FileHandler.write_file(filename, [asdict(txt) for txt in MemoryBuffer.logs])
