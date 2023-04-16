9f426914a4e58e5be833fac72fa6d16c7faed965
wszemart <wszeborska@wp.pl>
2023-04-16 14:22:57 +0200

    Add test method to save buffer to file

--- tests/test_memory_buffer.py
+++ tests/test_memory_buffer.py
@@ -1,19 +1,14 @@
-import tempfile
 from buffer import MemoryBuffer
 from text import Text
-from file_handler import FileHandler
-from unittest.mock import patch
 import json
-import os
-import pytest
 from unittest.mock import patch, mock_open
 from unittest import mock
 from dataclasses import asdict
+import os
 
 
 class TestMemoryBuffer:
-    @staticmethod
-    def test_create_log():
+    def test_create_log(self):
         MemoryBuffer.create_log(text="Sample text", rot_type="rot13", status="success")
         assert len(MemoryBuffer.logs) == 1
         assert isinstance(MemoryBuffer.logs[0], Text)
@@ -21,14 +16,12 @@ class TestMemoryBuffer:
         assert MemoryBuffer.logs[0].rot_type == "rot13"
         assert MemoryBuffer.logs[0].status == "success"
 
-    @staticmethod
-    def test_clear_buffer():
+    def test_clear_buffer(self):
         MemoryBuffer.create_log(text="Sample text", rot_type="rot13", status="success")
         MemoryBuffer.clear_buffer()
         assert len(MemoryBuffer.logs) == 0
 
-    @staticmethod
-    def test_del_position():
+    def test_del_position(self):
         MemoryBuffer.create_log(text="Sample text_1", rot_type="rot13", status="success")
         MemoryBuffer.create_log(text="Sample text_2", rot_type="rot47", status="fail")
         len_before_delete = len(MemoryBuffer.logs)
@@ -37,19 +30,22 @@ class TestMemoryBuffer:
         len_after_delete = len(MemoryBuffer.logs)
         assert len_before_delete - 1 == len_after_delete
 
-    @staticmethod
-    def test_save_buffer_to_file():
-        MemoryBuffer.create_log(text="Sample text", rot_type="rot13", status="success")
-        data = MemoryBuffer.logs[0]
+    def test_save_buffer_to_file(self):
+        # MemoryBuffer.create_log(text="Sample text", rot_type="rot13", status="success")
+        # log_data = [asdict(txt) for txt in MemoryBuffer.logs]
 
-        # read_data = json.dumps({"text": "Sample text", "rot_type": "rot13", "status": "success"})
-        # mock_open = mock.mock_open(read_data=read_data)
-        data = [asdict(txt) for txt in MemoryBuffer.logs]
         with patch('builtins.input', side_effect=['test_file', 'w']):
-            # with mock.patch('builtins.open', mock_open):
-            MemoryBuffer.save_buffer_to_file()
-            with mock.patch('builtins.open', mock_open):
-                with open('files/test_file') as file:
-                    data = json.load(file)
-        assert {"text": "Sample text", "rot_type": "rot13", "status": "success"} == data
-        
+            with mock.patch('builtins.open', mock_open()) as m:
+                print(m.mock_calls)
+                MemoryBuffer.create_log(text="Sample text", rot_type="rot13", status="success")
+                log_data = [asdict(txt) for txt in MemoryBuffer.logs]
+                MemoryBuffer.save_buffer_to_file()
+                with open('test_file', 'r') as f:
+                    saved_data = json.loads(f.read())
+                    print(f'{saved_data} o tu są dane których nie zapisuje!')
+                    assert saved_data == log_data
+
+        # with open('C:\\Users\\wszem\\Documents\\mentor\\caesar_cipher\\files\\test_file.json') as file:
+        #     data = json.load(file)
+        #
+        # assert log_data == data
